ec344b4bb77a4f43dfda2f8f9c9fc2e327d88deb
edgaras.spiridonovas <edgaras.spiridonovas@fltechnics.com>
2024-10-06 23:55:16 +0300

    **Restrict input types and enhance error handling**
    Define specific input types and add error styling to InputField component. This change improves the robustness of input handling and enhances user feedback through visual cues for errors. Added an `aria-label` for improved accessibility.

--- client/src/components/common/InputField/InputField.tsx
+++ client/src/components/common/InputField/InputField.tsx
@@ -1,4 +1,3 @@
-// InputField.tsx
 import React from "react";
 import styles from "./InputField.module.scss";
 
@@ -8,32 +7,34 @@ type InputFieldProps = {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   error?: string;
-  type?: string; // You can also restrict this to specific types (e.g., 'text', 'email', etc.)
+  type?: "text" | "email" | "password" | "number"; // Restrict type options
   placeholder?: string;
 };
 
 const InputField: React.FC<InputFieldProps> = ({
-  label,
-  name,
-  value,
-  onChange,
-  error,
-  type = "text",
-  placeholder,
-}) => {
+                                                 label,
+                                                 name,
+                                                 value,
+                                                 onChange,
+                                                 error,
+                                                 type = "text",
+                                                 placeholder,
+                                               }) => {
   return (
-    <div className={styles.inputGroup}>
-      <label htmlFor={name}>{label}</label>
-      <input
-        type={type}
-        value={value}
-        name={name}
-        onChange={onChange}
-        placeholder={placeholder}
-        id={name} // It's good practice to associate label with input
-      />
-      {error && <p className={styles.errorText}>{error}</p>}
-    </div>
+      <div className={styles.inputGroup}>
+        <label htmlFor={name}>{label}</label>
+        <input
+            type={type}
+            value={value}
+            name={name}
+            onChange={onChange}
+            placeholder={placeholder}
+            id={name}
+            aria-label={label} // Added for accessibility
+            className={error ? styles.errorInput : ''} // Add error class if there's an error
+        />
+        {error && <p className={styles.errorText}>{error}</p>}
+      </div>
   );
 };
 
