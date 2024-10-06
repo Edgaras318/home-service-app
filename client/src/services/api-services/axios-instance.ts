a46a9ade9c58cf0fa6f686dfa6feef34d7c76ea8
edgaras.spiridonovas <edgaras.spiridonovas@fltechnics.com>
2024-10-06 22:38:07 +0300

    Update token retrieval method in axios interceptor
    Modified the axios request interceptor to fetch the token from a nested field within 'user-storage' in localStorage. This update ensures the token is correctly parsed and set in the Authorization header, enhancing secure communication.

--- client/src/services/api-services/axios-instance.ts
+++ client/src/services/api-services/axios-instance.ts
@@ -9,16 +9,23 @@ const axiosInstance = axios.create({
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
     (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
-        // Retrieve the token from localStorage
-        const token = localStorage.getItem('token');
+        // Retrieve the 'user-storage' item from localStorage
+        const storedData = localStorage.getItem('user-storage');
 
-        // If token exists, add it to the Authorization header
-        if (token) {
-            // Create a new AxiosHeaders object and set the Authorization header
-            const headers = new AxiosHeaders(config.headers);
-            headers.set('Authorization', `Bearer ${token}`);
+        // If the data exists, parse it and extract the token
+        if (storedData) {
+            const parsedData = JSON.parse(storedData);
 
-            config.headers = headers;
+            // Check if parsedData has a 'token' and if so, set the Authorization header
+            const token = parsedData?.state?.token; // Assuming token is under 'state.token'
+
+            if (token) {
+                // Create a new AxiosHeaders object and set the Authorization header
+                const headers = new AxiosHeaders(config.headers);
+                headers.set('Authorization', `Bearer ${token}`);
+
+                config.headers = headers;
+            }
         }
 
         return config;
